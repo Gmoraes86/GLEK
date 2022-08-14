8fb3fe729b97cfd0389172674325162599affa35
Geferson de Moraes <gmoraes@enovati.com.br>
2022-08-14 02:01:32 -0300

    fix button stori to use theme provider

--- glek/Components/Base/Button/Button.stories.jsx
+++ glek/Components/Base/Button/Button.stories.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from '.';
+import ThemeProvider from '../../../Themes';
 
 export default {
   component: Button,  
 };
 
 
-const Template = () => <Button />;
+const Template = () => <ThemeProvider><Button /></ThemeProvider>;
 
 export const Primary = Template.bind({});
 export const Secondary = Template.bind({});
